86688b49606ee7cbc8d85d708f313a5fd065d3ed
zhanc1 <zhanc1@andrew.cmu.edu>
2016-09-30 22:34:38 -0400

    fix implementation bug: 1)illegal mutex operations will panic 2)illegal semaphore operations will panic

--- user/libthread/sem.c
+++ user/libthread/sem.c
@@ -3,21 +3,27 @@
 
 int sem_init( sem_t *sem, int count ){
 	sem->cnt = count;
+	sem->init_flag = 1;
+	sem->waiting = 0;
 	cond_init(&sem->cv);
 	mutex_init(&sem->cv_mutex);
 }
 
 void sem_wait( sem_t *sem ){
 	mutex_lock(&sem->cv_mutex);
+	assert(sem->init_flag);
 	while(sem->cnt <= 0){
+		sem->waiting++;
 		cond_wait(&sem->cv, &sem->cv_mutex);
 	}
+	sem->waiting--;
 	sem->cnt--;
 	mutex_unlock(&sem->cv_mutex);
 }
 
 void sem_signal( sem_t *sem ){
-	mutex_lock(&sem->cv_mutex);	
+	mutex_lock(&sem->cv_mutex);
+	assert(sem->init_flag);	
 	while(sem->cnt <= 0){
 		cond_signal(&sem->cv);
 	}
@@ -26,6 +32,9 @@ void sem_signal( sem_t *sem ){
 }
 
 void sem_destroy( sem_t *sem ){
+	mutex_lock(&sem->cv_mutex);
+	assert(sem->waiting == 0);
+	mutex_unlock(&sem->cv_mutex);
 	mutex_destroy(&sem->cv_mutex);
 	cond_destroy(&sem->cv);
 	sem->cnt = 0;
